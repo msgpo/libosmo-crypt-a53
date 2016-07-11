edacdfb5f423d48ecfea037dba74ee2da284b038
Max <msuraev@sysmocom.de>
2016-07-11 13:12:33 +0200

    Add deprecation note for functions available in libosmogsm

--- src/a53f.h
+++ src/a53f.h
@@ -1,8 +1,8 @@
 #ifndef A53_F_H
 #define A53_F_H
 
-void GSM( u8 *key, int klen, int count, u8 *block1, u8 *block2 );
+void GSM( u8 *key, int klen, int count, u8 *block1, u8 *block2 )  __attribute__((__deprecated__("Deprectaed: use implementation from libosmogsm instead")));
 void ECSD( u8 *key, int klen, int count, u8 *block1, u8 *block2 );
-void GEA3( u8 *key, int klen, u32 input, u8 direction, u8 *block, int m );
+void GEA3( u8 *key, int klen, u32 input, u8 direction, u8 *block, int m ) __attribute__((__deprecated__("Deprectaed: use implementation from libosmogsm instead")));
 
 #endif
